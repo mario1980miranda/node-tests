9ac8eb3cab9adeeb8c70a8050f5185f4f88df2df
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-09 13:55:16 -0300

    S06C57 - Organizing tests w/ describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,24 +3,34 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('should return Hello World response', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hello World')
-        .end(done);
-    ;
-});
-
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Ozzy',
-                age: 68
+describe('Server Tests', () => {
+    describe('GET /', () => {
+        describe('Verify if page return \'Hello World\'', () => {
+            it('should return Hello World response', (done) => {
+                request(app)
+                    .get('/')
+                    .expect(200)
+                    .expect('Hello World')
+                    .end(done);
+                ;
             });
-        })
-        .end(done);
+        });
+    });
+    describe('GET /users', () => {
+        describe('Verify if a especific user is in the list', () => {
+            it('should return my user object', (done) => {
+                request(app)
+                    .get('/users')
+                    .expect(200)
+                    .expect((res) => {
+                        expect(res.body).toInclude({
+                            name: 'Ozzy',
+                            age: 68
+                        });
+                    })
+                    .end(done)
+                ;
+            });
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,51 +1,64 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-    var results = utils.add(33, 11);
-    expect(results).toBe(44).toBeA('number');
-});
-
-it('should square a number', () => {
-    var results = utils.square(3);
-    expect(results).toBe(9).toBeA('number');
-});
-
-it('should expect some values', () => {
-    // expect(12).toNotBe(12);
-    // expect({name: 'Mario'}).toEqual({name: 'Mario'});
-    // expect({name: 'Mario'}).toNotEqual({name: 'mario'});
-    // expect([2,3,4]).toInclude(5);
-    // expect([2,3,4]).toInclude(2);
-    // expect([2,3,4]).toExclude(1);
-    expect({
-        name: 'Mario',
-        age: 37,
-        location: 'Curitiba'
-    }).toInclude({
-        age: 37
+describe('Utils', () => {
+    describe('Synchronous methods', () => {
+        it('should add two numbers', () => {
+            var results = utils.add(33, 11);
+            expect(results).toBe(44).toBeA('number');
+        });
+        it('should square a number', () => {
+            var results = utils.square(3);
+            expect(results).toBe(9).toBeA('number');
+        });
+    });
+    describe('Assynchronous methods', () => {
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sumResult) => {
+                expect(sumResult).toBe(7).toBeA('number');
+                done();
+            });
+        });
+        it('should async square a number', (done) => {
+            utils.asyncSquare(3, (squareResult) => {
+                expect(squareResult).toBe(9).toBeA('number');
+                done();
+            });
+        });
     });
 });
 
-it('should verify the firs and las names are set', () => {
-    var user = {location: 'Parana', age: 37};
-    var results = utils.setName(user, 'Mario Miranda');
-    expect(results).toInclude({
-        firstName: 'Mario',
-        lastName: 'Miranda'
+describe('Playground tests', () => {
+    it('should expect some values', () => {
+        expect({
+            name: 'Mario',
+            age: 37,
+            location: 'Curitiba'
+        }).toInclude({
+            age: 37
+        });
     });
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sumResult) => {
-        expect(sumResult).toBe(7).toBeA('number');
-        done();
+    it('should verify the firs and las names are set', () => {
+        var user = {location: 'Parana', age: 37};
+        var results = utils.setName(user, 'Mario Miranda');
+        expect(results).toInclude({
+            firstName: 'Mario',
+            lastName: 'Miranda'
+        });
+    });      
+    it('should not be equals 12', () => {
+        expect(21).toNotBe(12);
     });
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(3, (squareResult) => {
-        expect(squareResult).toBe(9).toBeA('number');
-        done();
+    it('should be equals comparing objects', () => {
+        expect({name: 'Mario'}).toEqual({name: 'Mario'});
+    });
+    it('should not be equals', () => {
+        expect({name: 'Mario'}).toNotEqual({name: 'mario'});
+    });
+    it('should to be in the list', () => {
+        expect([2,3,4]).toInclude(2);
+    });
+    it('should not to be in the list', () => {
+        expect([2,3,4]).toExclude(1);
     });
 });
