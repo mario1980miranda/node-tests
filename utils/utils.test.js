d7d457ed3cc2f57d96e8b8cd23b9ee0e0ca92bc0
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-09 10:50:06 -0300

    S05-C53 - Assertions w/ 3º party library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,37 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     var results = utils.add(33, 11);
-
-    if (results !== 44) {
-        throw new Error('Expected 44, but got ' + results);
-    }
+    expect(results).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
-    var results = utils.square(2);
-
-    if (results !== 4) {
-        throw new Error(`Expected 4, but got ${results}`);
-    }
+    var results = utils.square(3);
+    expect(results).toBe(9).toBeA('number');
 });
+
+it('should expect some values', () => {
+    // expect(12).toNotBe(12);
+    // expect({name: 'Mario'}).toEqual({name: 'Mario'});
+    // expect({name: 'Mario'}).toNotEqual({name: 'mario'});
+    // expect([2,3,4]).toInclude(5);
+    // expect([2,3,4]).toInclude(2);
+    // expect([2,3,4]).toExclude(1);
+    expect({
+        name: 'Mario',
+        age: 37,
+        location: 'Curitiba'
+    }).toInclude({
+        age: 37
+    });
+});
+
+it('should verify the firs and las names are set', () => {
+    var user = {location: 'Parana', age: 37};
+    var results = utils.setName(user, 'Mario Miranda');
+    expect(results).toInclude({
+        firstName: 'Mario',
+        lastName: 'Miranda'
+    });
+})
